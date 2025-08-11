f3174cde6d01717bd2e067d2ab8b2605a52be5fe
DEXTER-ID-KING <dexternew785@gmail.com>
2025-08-11 10:56:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   DATABASE_URL: "postgresql://bot_whatsapp_g3pd_user:Sip0HTRwgHlGi2lZcqX2fJ0ajLB0e4ut@dpg-d2clk0hr0fns73e28r5g-a.oregon-postgres.render.com/bot_whatsapp_g3pd",
   PORT: 9090,
-  SESSION_ID: "DEXTER-ID=FKt0VJzL#xgGjuvyceA_2zxZakGOIp0YpmvlC7GS2s_dhDty5UsI",
+  SESSION_ID: "DEXTER-ID=cLFBgahY#_wuBnrByqtpiI5gyyS2IJsdVSjOH-mZhUUCwOxgb44Q",
   OWNER_NUMBER: ["94789958225"], // Add your number here
   READ_MESSAGE: false,
   AUTO_STATUS_SEEN: true,
