100cbda5505b84c0c0411d136773f2702a2075d3
DEXTER-ID-KING <dexternew785@gmail.com>
2025-12-24 06:00:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  DATABASE_URL: "postgresql://bot_whatsapp_edsa_user:w4XyVM0IskSbmWKgdq0A6DzZFLalhpP2@dpg-d2v8dh8gjchc73av8h40-a.oregon-postgres.render.com/bot_whatsapp_edsa",
+  DATABASE_URL: "postgresql://bot_9wh3_user:exPHB4hI0wsBKHjQTkcxmMtpF4RffuhU@dpg-d55j7gu3jp1c739tb5eg-a.virginia-postgres.render.com/bot_9wh3",
   PORT: 9090,
   SESSION_ID: "DEXTER-ID=XdtziJaB#DXgclLM1PDc7fEeBPGqVOcKvTaEDOvsVvNq-auSqwBA",
   OWNER_NUMBER: ["94789958225"], // Add your number here
   READ_MESSAGE: false,
   AUTO_STATUS_SEEN: true,
-  AUTO_STATUS_REACT: true,
+  AUTO_STATUS_REACT: false,
   PREFIX: "!",
   RENDER_JSON_URL: 'https://status-seen-and-react-only.onrender.com/json',
   IMGBB_API_KEY: 'ad205a046fdf200c4013a34b033e646f' // Obtain from https://api.imgbb.
